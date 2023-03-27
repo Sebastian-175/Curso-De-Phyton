ff0e1abe732f9fc3e1c790f7ff7f1c540c5b9a19
Sebastian-175 <128272901+Sebastian-175@users.noreply.github.com>
2023-03-27 11:11:14 -0500

    Add files via upload

--- files.py
+++ files.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 27 10:58:49 2023
+Created on Mon Mar 27 11:05:07 2023
 
 @author: usuario
 """
 
+lista=[]
 archivo=open("devices.txt")
 for item in archivo:
+    item=item.strip()
+    lista.append(item)
     print(item)
 archivo.close()
